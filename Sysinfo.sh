ba31025b21c69c42c5f36b054a58916215591312
evan1254 <e3365103@student.dcccd.edu>
2025-04-23 12:12:01 -0500

    Create Sysinfo.sh

---
+++ Sysinfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Basic Script To Gather System Information In RedHat
+
+echo "====== System Information ======"
+echo "Hostname: $(hostname)"
+echo "OS Version: $(cat /etc/redhat-release)"
+echo
+echo "=== CPU Information ==="
+lscpu | grep -E '^CPU\(s\):|^Model name:'
+echo
+echo "=== Memory Usage ==="
+free -h | grep -E 'Mem:'
+echo
+echo "=== Disk Usage ==="
+df -h / | grep -v Filesystem
+echo "=========================="
